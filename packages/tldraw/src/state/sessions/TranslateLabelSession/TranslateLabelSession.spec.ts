149f6965240e351264953a6789615c9ea185e375
Komputer-P <wlghks0314@gmail.com>
2022-05-13 03:45:13 +0900

    tldrawhiteboard yjs-tldraw web application implemented

--- packages/tldraw/src/state/sessions/TranslateLabelSession/TranslateLabelSession.spec.ts
+++
@@ -1,7 +0,0 @@
-import { mockDocument, TldrawTestApp } from '~test'
-import { SessionType, TDShapeType, TDStatus } from '~types'
-
-describe('Translate label session', () => {
-  it.todo('begins, updateSession')
-  it.todo('cancels session')
-})
--- packages/tldraw/src/state/shapes/shared/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './getBoundsRectangle'
-export * from './transformRectangle'
-export * from './transformSingleRectangle'
-export * from './TextAreaUtils'
-export * from './shape-styles'
-export * from './getTextAlign'
-export * from './TextLabel'
